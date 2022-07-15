38a7e1c0dc3549636640e91bcb7c63a2af685fe3
Rui Wang <rui.wang.624@gmail.com>
2022-07-15 12:16:47 -0400

    custom icon completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'custom_icon';
+  title = 'custom icon';
+
+  constructor (
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'lobster',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/lobster.svg')
+    )
+
+  }
 }
